07a2a4a0cacee7b62c90e93304f61aee20fc1888
Ryutaro Matsumoto <taross0524.ss@gmail.com>
2020-11-01 18:19:15 +0900

    removed test data

--- src/templates/BlogLayout.tsx
+++ src/templates/BlogLayout.tsx
@@ -2,13 +2,6 @@ import React from 'react'
 import BlogHeader from '@/components/BlogHeader'
 import Footer from '@/components/Footer'
 
-const testData = {
-  title: 'Neovimの設定を晒してみる',
-  description: '自分が普段使っているNeovimの設定を解説します',
-  postedAt: '2020/09/14',
-  tags: ['Neovim', 'Editor', 'VimScript', 'Test'],
-}
-
 type Props = {
   title: string
   description: string
